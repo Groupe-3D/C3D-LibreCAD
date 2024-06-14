86b0bad5a02dbb793f399f9e836748f224e78e36
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2024-06-14 12:17:51 +0200

    Make parsing work, needs data exchange code

--- librecad/src/main/console_dxf2vec/vec_converter_loop.cpp
+++ librecad/src/main/console_dxf2vec/vec_converter_loop.cpp
@@ -28,9 +28,10 @@
 #include <QtCore>
 
 #include "rs.h"
+#include "rs_block.h"
 #include "rs_blocklist.h"
 #include "rs_graphic.h"
-#include "rs_painterqt.h"
+#include "rs_insert.h"
 #include "rs_polyline.h"
 #include "rs_units.h"
 
@@ -67,27 +68,46 @@ void VecConverterLoop::convertOneDxfToOneVec(const QString &dxfFile)
 
     QList<QList<QVector3D>> allPolylinesPoints;
 
-    for (auto entity : *graphic) {
+    auto processEntity = [&allPolylinesPoints, &graphic](RS_Entity *entity,
+                                                         auto &&processEntityRef) -> void {
         if (entity->rtti() == RS2::EntityPolyline) {
             RS_Polyline *polyline = static_cast<RS_Polyline *>(entity);
             QList<QVector3D> polylinePoints;
-
-            for (auto v : polyline->getRefPoints()) {
-                polylinePoints.append(QVector3D(v.x, v.y, 0.0));
+            for (const RS_Entity *e : *polyline) {
+                if (e->rtti() == RS2::EntityLine || e->rtti() == RS2::EntityArc) {
+                    RS_Vector startPoint = e->getStartpoint();
+                    polylinePoints.append(QVector3D(startPoint.x, startPoint.y, startPoint.z));
+                }
             }
             allPolylinesPoints.append(polylinePoints);
+        } else if (entity->rtti() == RS2::EntityInsert) {
+            RS_Block *block = graphic->getBlockList()->find(
+                static_cast<RS_Insert *>(entity)->getName());
+            if (block) {
+                for (RS_Entity *subEntity : *block) {
+                    processEntityRef(subEntity, processEntityRef);
+                }
+            }
         }
+    };
+
+    for (RS_Entity *entity : *graphic) {
+        processEntity(entity, processEntity);
     }
 
     // DEBUG
+    int polylineCount = 0;
+
     for (const auto &polylinePoints : allPolylinesPoints) {
-        qDebug() << "Polyline:";
+        qDebug() << "Polyline no=" << polylineCount++ << ":";
         for (const auto &point : polylinePoints) {
             qDebug() << point;
         }
     }
 
     qDebug() << "Printing" << dxfFile << "to" << params.outFile << "DONE";
+
+    // TODO exchange data with shared memory buffer here, or use .vec
 
     delete doc;
 }
@@ -107,13 +127,7 @@ void VecConverterLoop::convertManyDxfToOneVec() {
     }
 
     QVector<DxfPage> pages;
-    int nrPages = 0;
 
-    // FIXME: Should probably open and print all dxf files in one 'for' loop.
-    // Tried but failed to do this. It looks like some 'chicken and egg'
-    // situation for the QPrinter and RS_PainterQt. Therefore, first open
-    // all dxf files and apply required actions. Then run another 'for'
-    // loop for actual printing.
     for (auto &dxfFile : params.dxfFiles) {
         DxfPage page;
 
@@ -128,39 +142,10 @@ void VecConverterLoop::convertManyDxfToOneVec() {
 
         pages.append(page);
 
-        nrPages++;
+        //nrPages++;
     }
 
-    QPrinter printer(QPrinter::HighResolution);
-
-    if (nrPages > 0) {
-        // FIXME: Is it possible to set up printer and paper for every
-        // opened dxf file and tie them with painter? For now just using
-        // data extracted from the first opened dxf file for all pages.
-        //setupPrinterAndPaper(pages.at(0).graphic, printer, params);
-    }
-
-    RS_PainterQt painter(&printer);
-
-    // And now it's time to actually print all previously opened dxf files.
-    for (auto &page : pages) {
-        nrPages--;
-
-        qDebug() << "Printing" << page.dxfFile
-                 << "to" << params.outFile << ">>>>";
-
-        //drawPage(page.graphic, printer, painter);
-
-        qDebug() << "Printing" << page.dxfFile
-                 << "to" << params.outFile << "DONE";
-
-        delete page.doc;
-
-        if (nrPages > 0)
-            printer.newPage();
-    }
-
-    painter.end();
+    // TODO
 }
 
 
